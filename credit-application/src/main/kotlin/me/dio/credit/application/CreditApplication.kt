bc41e782c479ccafb179cfca9488d9ed51d2e223
Luiz Henrique <luizhenriquegithub@gmail.com>
2023-03-27 12:45:34 -0300

    first commit main

---
+++ credit-application/src/main/kotlin/me/dio/credit/application/CreditApplication.kt
@@ -0,0 +1,11 @@
+package me.dio.credit.application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplication>(*args)
+}
